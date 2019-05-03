099fdd9df40d73bfac5d94dd49e1cbfca78ac26c
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2019-05-03 07:58:25 +0700

    Python for data scientist; dataframe using pandas

---
+++ Python/Datacamp/Intermediate Python for Data Science/dictionaries.py
@@ -0,0 +1,149 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital': 'rome', 'population': 59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+
+
+############# PANDAS ##############
+
+"""
+Removing duplicate rows		df.drop_duplicates()
+Pivot						df.pivot(values, columns, index)
+"""
+# Import pandas as pd
+import pandas as pd
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
+
+# Build a DataFrame cars from my_dict: cars
+cars = pd.DataFrame(my_dict)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JAP', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars
+## Notice that the columns of cars can be of different types. This was not possible with 2D Numpy arrays!
+print(cars)
+
+
+"""
+Square brackets
+	Column access
+		brics[["country", "capital"]]
+	Row access
+		brics[1:4]
+
+Loc (label-based)
+	Row Access
+		brics.loc[["RU", "IN", "CH"]]
+	Column access
+		brics.loc[:, ["country", "capital"]]
+	Row & Column access
+		brics.loc[["RU", "IN", "CH"], ["country", "capital"]]
+
+Iloc (index-based)
+	Row access
+		brics.iloc[[1, 2, 3]]
+"""
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(cars["country"])
+
+# Print out country column as Pandas DataFrame
+print(cars[["country"]])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[["country", "drives_right"]])
+
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR', 'drives_right'])
+
+# Print sub-DataFrame
+print(cars.iloc[[4, 5], [1, 2]])
+
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars.iloc[:, 2])
+
+# Print out drives_right column as DataFrame
+print(cars.iloc[:, [2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
