ed300d6bb6921ae5c148e4b301ead19870b04398
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2019-05-16 19:58:44 +0700

    creating function for a neat script sentinel downloading

--- Python/EO/sentinelapi.py
+++ Python/EO/sentinelapi.py
@@ -26,86 +26,111 @@ query_geom = [g.wkt for g in queryTileSentinel['geometry'].values]
 tile = gpd.read_file("tile_100km.shp")
 geom = [g.wkt for g in tile['geometry'].values]
 
-user = 'rizkyfirmansyah'
-password = 'WRIesa2018!'
+cre = {'u': 'rizkyfirmansyah', 'p': 'WRIesa2018!'}
 
 ## Set your query parameters here
 CC = 100
 sentinelLevel1 = 'Level-1C'
 sentinelLevel2 = 'Level-2A'
-t1 = '20180101'
-t2 = '20181231'
+a1 = '20180101'
+#t2 = '20190516'
+a2 = '20181231'
 
-api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
+api = SentinelAPI(cre['u'], cre['p'], 'https://scihub.copernicus.eu/dhus')
+
+products1 = ""
+products2 = ""
 
 def sentinel_metadata(d, e):
-    """
-    d for selecting your Sentinel product
-    while e for creating a new file containing title, date and cloud coverage
-    """
-    header = ['filename', 'sensing_start', 'cloud_cover', 'size', 'orbit']
-    line_count = 0
-    for key in d.keys():
-        for v in key.split():
-            title = d[v]['title']
-#            date = d[v]['datatakesensingstart']
-            date = d[v]['beginposition']
-            cc = d[v]['cloudcoverpercentage']
-            size = d[v]['size']
-            orbit = d[v]['orbitdirection']
-            f = None
-            try:
-                with open(e, "a") as f:
-                    if line_count == 0:
-                        print(header[0], header[1], header[2], header[3], header[4], sep=', ', file = f)
-                        line_count += 1
-                    else:
-                        print(title, date, cc, size, orbit, sep=', ', file = f)
-                        line_count += 1
-            finally:
-                if f is not None:
-                    f.close()
-        f.close()
+        """
+        d for selecting your Sentinel product
+        while e for creating a new file containing title, date and cloud coverage
+        """
+        header = ['filename', 'begin_position', 'cloud_cover', 'size', 'orbit']
+        line_count = 0
+        for key in d.keys():
+            for v in key.split():
+                title = d[v]['title']
+                date = d[v]['beginposition']
+                cc = d[v]['cloudcoverpercentage']
+                size = d[v]['size']
+                orbit = d[v]['orbitdirection']
+                f = None
+                try:
+                    with open(e, "a") as f:
+                        if line_count == 0:
+                            print(header[0], header[1], header[2], header[3], header[4], sep=', ', file = f)
+                            line_count += 1
+                        else:
+                            print(title, date, cc, size, orbit, sep=', ', file = f)
+                            line_count += 1
+                finally:
+                    if f is not None:
+                        f.close()
+            f.close()
 
-n = 78
-while n < len(geom):
-    products1 = api.query(geom[n],
-                         date = (t1, t2),
-                         platformname = 'Sentinel-2',
-                         processinglevel = sentinelLevel1,
-                         cloudcoverpercentage = (0, CC))
+def download_metadata(t1, t2, l1 = True, l2 = False):
+    """ Download metadata Sentinel 2 either L1A or L2C"""
+
+    global products1
+    global products2
+    # n starts with 0
+    n = 0
+    while n < len(geom):
+        products1 = api.query(geom[n],
+                             date = (t1, t2),
+                             platformname = 'Sentinel-2',
+                             processinglevel = sentinelLevel1,
+                             cloudcoverpercentage = (0, CC))
+        
+        products2 = api.query(geom[n],
+                             date = (t1, t2),
+                             platformname = 'Sentinel-2',
+                             processinglevel = sentinelLevel2,
+                             cloudcoverpercentage = (0, CC))
+        try:
+            if l1 == True and l2 == True:
+                sentinel_metadata(products1, 'Sentinel2_' + sentinelLevel1 + '_' + t1 + '_' + t2 + '_' + 'tile' + str(n+1) + '.csv')
+                sentinel_metadata(products2, 'Sentinel2_' + sentinelLevel2 + '_' + t1 + '_' + t2 + '_' + 'tile' + str(n+1) + '.csv')
+            elif l1 == True:
+                sentinel_metadata(products1, 'Sentinel2_' + sentinelLevel1 + '_' + t1 + '_' + t2 + '_' + 'tile' + str(n+1) + '.csv')
+            elif l2 == True:
+                sentinel_metadata(products2, 'Sentinel2_' + sentinelLevel2 + '_' + t1 + '_' + t2 + '_' + 'tile' + str(n+1) + '.csv')
+        except:
+            sleep(30)
+
+        n += 1
+
+areas = api.to_geodataframe(products1)
+
+def view_aoi():
+    """ Return the Sentinel tiles over the area of interest. Choose the products either L1C or L2A"""
+    areas.plot(column = 'uuid', cmap=None)
     
-    products2 = api.query(geom[n],
-                         date = (t1, t2),
-                         platformname = 'Sentinel-2',
-                         processinglevel = sentinelLevel2,
-                         cloudcoverpercentage = (0, CC))
-    try:
-#        sentinel_metadata(products1, 'Sentinel2_' + sentinelLevel1 + '_' + t1 + '_' + t2 + '_' + 'tile' + str(n+1) + '.csv')
-        sentinel_metadata(products2, 'Sentinel2_' + sentinelLevel2 + '_' + t1 + '_' + t2 + '_' + 'tile' + str(n+1) + '.csv')
-    except:
-        sleep(30)
-    n += 1
+    ax = areas.plot(column = 'uuid', cmap=None, figsize=(20,20))
+    areas.apply(lambda x: ax.annotate(s=x.uuid, xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
 
-#
-#areas = api.to_geodataframe(products1)
-#areas.plot(column = 'uuid', cmap=None)
-#
-#ax = areas.plot(column = 'uuid', cmap=None, figsize=(20,20))
-#areas.apply(lambda x: ax.annotate(s=x.uuid, xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
+def scene_download():
+    """ use geopandas to check which scene to download """
+    gdf2 = gpd.read_file(geom)
+    f, ax = plt.subplots(1)
+    areas.plot(ax=ax, column='uuid', cmap=None)
+    gdf2.plot(ax=ax)
+    plt.show()
 
-# use geopandas to check which scene to download
-#gdf2 = gpd.read_file(aoi)
-#f, ax = plt.subplots(1)
-#areas.plot(ax=ax, column='uuid', cmap=None)
-#gdf2.plot(ax=ax)
-#plt.show()
+def download_data():
+    """ download all the data. Choose the download_all either Level 1C or Level 2A"""
+    # n starts with 0
+    n = 0
+    while n < len(geom):
+        try:
+            data = api.download_all(products1)
+            
+            path_tozip = data['path']
+            
+            zip_ref = zipfile.ZipFile(path_tozip, 'r')
+            zip_ref.extractall(r'D:/DATA/GEOSPATIAL/OPEN DATA/Sentinel')
+            zip_ref.close()
+        except:
+            print("There's something wrong with your connection")
 
-## download all the data
-#data = api.download_all(products)
-#
-#path_tozip = data['path']
-#
-#zip_ref = zipfile.ZipFile(path_tozip, 'r')
-#zip_ref.extractall(r'D:/DATA/GEOSPATIAL/OPEN DATA/Sentinel')
-#zip_ref.close()
